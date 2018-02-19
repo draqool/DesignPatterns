eb2c2865824586b113f67c3e81e6ac1aa4196f7d
deadzone747 <deadzone747@gmail.com>
2018-02-19 22:14:13 +0100

    Singleton init

---
+++ Singleton/Logger.cs
@@ -0,0 +1,28 @@
+﻿namespace Singleton
+{
+    public sealed class Logger
+    {
+        private static readonly Logger instance = new Logger();
+
+        static Logger()
+        {
+        }
+
+        private Logger()
+        {
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void Log()
+        {
+            System.Console.WriteLine("log");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Instance.Log();
+            Console.ReadKey();
+        }
+    }
+}
