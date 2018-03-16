1e634509f92c026b105528f143f33e2f692415c1
trytocatchme <deadzone747@gmail.com>
2018-03-16 21:00:24 +0100

    added multiply and add oepration

---
+++ Strategy/OperationAdd.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Strategy
+{
+    public class OperationAdd : IOperation
+    {
+        public int Calculate(int val1, int val2)
+        {
+            return num1 + num2;
+        }
+    }
+}
