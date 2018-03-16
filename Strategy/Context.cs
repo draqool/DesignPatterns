9d2a3258d8541c9d9c37c57f3596d3a4ccc426c2
trytocatchme <deadzone747@gmail.com>
2018-03-16 21:10:04 +0100

    add context

---
+++ Strategy/Context.cs
@@ -0,0 +1,17 @@
+﻿namespace Strategy
+{
+    public class Context
+    {
+        private IOperation _operation;
+
+        public Context(IOperation operation)
+        {
+            _operation = operation;
+        }
+
+        public int executeStrategy(int num1, int num2)
+        {
+            return _operation.Calculate(num1, num2);
+        }
+    }
+}
