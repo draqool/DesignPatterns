1022e1ac28ed0af7b374c6bdcc6e458c3c6d8073
trytocatchme <deadzone747@gmail.com>
2018-03-19 20:38:34 +0100

    init reducemap

---
+++ ReduceMap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReduceMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
