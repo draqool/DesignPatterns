813dac4c257979be128f107475de9bacfb5fbfea
trytocatchme <deadzone747@gmail.com>
2018-03-19 20:59:57 +0100

    add some fake data in person model

--- ReduceMap/Person.cs
+++ ReduceMap/Person.cs
@@ -1,8 +1,24 @@
-﻿namespace ReduceMap
+﻿using System.Collections.Generic;
+
+namespace ReduceMap
 {
     public class Person
     {
-        public int Name { get; set; }
+        public string Name { get; set; }
         public int Age { get; set; }
+
+        public List<Person> GetListOfPerson
+        {
+            get
+            {
+                return new List<Person>()
+                {
+                    new Person { Name = "Artur", Age = 11 },
+                    new Person { Name = "Adam", Age = 19 },
+                    new Person { Name = "Karol", Age = 40 },
+                    new Person { Name = "Bartek", Age = 30 }
+                };
+            }
+        }
     }
 }
