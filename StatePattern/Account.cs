06b791fb7b1ad939d7dfe5ea42103747e8c15bb4
trytocatchme <deadzone747@gmail.com>
2018-03-25 21:23:23 +0200

    added account

---
+++ StatePattern/Account.cs
@@ -0,0 +1,30 @@
+﻿using StatePattern.AccountStates;
+using System;
+
+namespace StatePattern
+{
+    public class Account
+    {
+        private IAccountState _state;
+        public decimal Balance { get; private set; }
+       
+        public Account(Action onUnfreeze)
+        {
+            _state = new NotVerified(onUnfreeze);
+        }
+
+        public void Deposit(decimal amount)
+        {
+            _state = this._state.Deposit(() => { this.Balance += amount; });
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            _state = _state.Withdraw(() => { this.Balance -= amount; });
+        }
+
+        public void HolderVerified() => _state = _state.HolderVerified();
+        public void Close() => _state = _state.Close();    
+        public void Freeze() => _state = _state.Freeze();
+    }
+}
