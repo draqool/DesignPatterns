eb8743f2d3fa0049808096ca5442330fec215317
trytocatchme <deadzone747@gmail.com>
2018-05-02 22:02:28 +0200

    criteria

---
+++ FilterPattern/CriteriaOperator/AndCriteria.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace FilterPattern.CriteriaOperator
+{
+    public class AndCriteria : ICriteria
+    {
+        private readonly ICriteria criteria;
+        private readonly ICriteria otherCriteria;
+
+        public AndCriteria(ICriteria criteria, ICriteria otherCriteria)
+        {
+            this.criteria = criteria;
+            this.otherCriteria = otherCriteria;
+        }
+
+        public List<Person> MeetCriteria(List<Person> persons)
+        {
+            List<Person> firstCriteriaPersons = criteria.MeetCriteria(persons);
+            return otherCriteria.MeetCriteria(firstCriteriaPersons);
+        }
+    }
+}
---
+++ FilterPattern/CriteriaOperator/OrCriteria.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace FilterPattern.CriteriaOperator
+{
+    public class OrCriteria : ICriteria
+    {
+        private readonly ICriteria criteria;
+        private readonly ICriteria otherCriteria;
+
+        public OrCriteria(ICriteria criteria, ICriteria otherCriteria)
+        {
+            this.criteria = criteria;
+            this.otherCriteria = otherCriteria;
+        }
+
+        public List<Person> MeetCriteria(List<Person> persons)
+        {
+            List<Person> firstCriteriaItems = criteria.MeetCriteria(persons);
+            List<Person> otherCriteriaItems = otherCriteria.MeetCriteria(persons);
+
+            foreach (var person in otherCriteriaItems)
+            {
+                if (!firstCriteriaItems.Contains(person))
+                {
+                    firstCriteriaItems.Add(person);
+                }
+            }
+
+            return firstCriteriaItems;
+        }
+    }
+}
